7c0a7bd422da0f424da3623eaab0a3b86bae316b
Paolla Olsen <paollaolsen@gmail.com>
2020-02-09 14:17:24 -0300

    Download images from instagram

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-#import requests
-#from bs4 import BeautifulSoup
-
-#page = requests.get('https://www.instagram.com/paollaolsen/')
-#soup = BeautifulSoup(page.text, 'html.parser')
-
-# artist_name_list = soup.find(class_='BodyText')
-# artist_name_list_items = artist_name_list.find_all('a')
-
-# print(soup)
-
-#item = soup.find('div', {'class':'v1Nh3 kIKUG  _bz0w'})
-
-#print(item)
-
-#link = item.find_all('a')
-
-#print(link)
-
 from selenium import webdriver
 
 import time
 import numpy as np
+import requests
+import urllib.request
 
 browser = webdriver.Firefox()
 browser.implicitly_wait(10)
@@ -33,15 +16,9 @@ print(ifLogged)
 while ifLogged == 0:
    print('Not exists')
    ifLogged = len(browser.find_elements_by_css_selector('._2dbep.qNELH.kIKUG'))
-   # print(ifLogged)
 
 browser.get('https://www.instagram.com/paollaolsen/')
 browser.implicitly_wait(10)
-
-# quantityPosts = int(browser.find_element_by_css_selector('.g47SY').text)
-# print(quantityPosts)
-
-# quantityPostsAtual = 0
 
 content = browser.find_elements_by_css_selector('.v1Nh3 a')
 
@@ -60,19 +37,27 @@ while True:
         browser.execute_script("arguments[0].click();", item)
         if browser.find_elements_by_css_selector('.PdwC2 .FFVAD'):
             photo = browser.find_element_by_css_selector('.PdwC2 .FFVAD')
-            link = photo.get_attribute("src")
-            photos.append(link)
         if browser.find_elements_by_css_selector('.PdwC2 .tWeCl'):
-            video = browser.find_element_by_css_selector('.PdwC2 .tWeCl')
-            link = video.get_attribute("src")
-            photos.append(link)
-    # print(photos)
-    # print('***')
+            photo = browser.find_element_by_css_selector('.PdwC2 .tWeCl')
+        link = photo.get_attribute("src")
+        photos.append(str(link))
     if new_height == last_height:
         break
     last_height = new_height
 
 photos = np.unique(photos)
 
-#print(photos)
 print(len(photos))
+
+np.savetxt("data.txt", photos, fmt="%s")
+
+f = open("data.txt","r")
+if f.mode == "r":
+    lines = f.readlines()
+    i = 0
+    for line in lines:
+        i += 1
+        urllib.request.urlretrieve(line, str(i) + '.png')
+        print(i)
+
+f.close()
